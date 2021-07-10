28cadca23798e76833a5a89babfa8416c25277ab
Phil Cole <filcole@gmail.com>
2021-07-10 11:34:40 +0100

    Avoid powerapps checker strict equality issues

--- code/clientjs/webpack.prod.js
+++ code/clientjs/webpack.prod.js
@@ -3,4 +3,19 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+        new TerserPlugin({
+            terserOptions: {
+                compress: {
+                    // Power Apps Solution Checker will complain if strict equality operators
+                    // are optimised, rule: web-use-strict-equality-operators
+                    // see https://docs.microsoft.com/en-us/powerapps/maker/data-platform/use-powerapps-checker#best-practice-rules-used-by-solution-checker
+                    comparisons: false
+                }
+            }
+        })
+    ]
+  }  
 });
